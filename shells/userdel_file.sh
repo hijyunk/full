1c5e1098970573f727e2fb8726ec38017dc4ef72
hijyunk <ziggzagg@ajou.ac.kr>
2024-03-15 17:00:12 +0900

    shell scripts

---
+++ shells/userdel_file.sh
@@ -0,0 +1,15 @@
+#1/bin/bash
+
+input="user.dat"
+
+while IFS=',' read -r username uid gid comment
+do
+	userdel "$username"
+	rm -rf /home/$username
+	rm -rf /var/mail/$username
+	echo "Deleting $username"
+done < $input
+
+echo 
+tail -3 /etc/passwd
+
